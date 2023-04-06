e5b96ee78001912cfbce3b40bee59061ea7b6cdf
ka1x <kath021@interia.pl>
2023-04-06 13:25:57 +0200

    note content styling

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,17 +5,20 @@ const Note = ({note}) => {
    return (
       <>
          <div className='note-header-container'>
-            <p className='note-date'>{new Date(note.date).toLocaleDateString('en-GB', {weekday: 'long', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'})}</p>
+            <p className='note-date'>{new Date(note.date).toLocaleDateString('en-GB', {month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'})}</p>
 
-            <p className='note-title'>title:{note.title}</p>
+            <p className='note-title'>{note.title}</p>
 
             <div className='line'></div>
          </div>
 
          <div className='note-content-container'>
             {/* <p>content:{note.content}</p> */}
-            <p className='note-content'>{note.content && note.content.length > 50 ? note.content.substr(0, 50) + '...' : note.content}</p>
+            <p className='note-content'>{note.content && note.content.length > 60 ? note.content.substr(0, 60) + '...' : note.content}</p>
          </div>
+
+
+
       </>
    );
 };
