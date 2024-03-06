5b1ed02341b6c321690324f039721e6ca80b4085
ka1x <kath021@interia.pl>
2024-03-06 15:36:58 +0100

    note shape change

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -7,14 +7,14 @@ const Note = ({note}) => {
          <div className='note-header-container'>
             <p className='note-date'>{new Date(note.date).toLocaleDateString('en-GB', {month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'})}</p>
 
-            <p className='note-title'>{note.title === '' ? 'Untitled' : note.title.length > 35 ? note.title.substr(0, 60) + '...' : note.title}</p>
+            <p className='note-title'>{note.title === '' ? 'Untitled' : note.title.length > 35 ? note.title.substr(0, 35) + '...' : note.title}</p>
 
             <div className='line'></div>
          </div>
 
          <div className='note-content-container'>
             {/* <p>content:{note.content}</p> */}
-            <p className='note-content'>{note.content && note.content.length > 60 ? note.content.substr(0, 60) + '...' : note.content}</p>
+            <p className='note-content'>{note.content && note.content.length > 200 ? note.content.substr(0, 200) + '...' : note.content}</p>
          </div>
       </>
    );
