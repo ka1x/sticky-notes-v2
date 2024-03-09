6a73b1cf885c8c53c83fe418ff21f14879fa0b9e
ka1x <kath021@interia.pl>
2024-03-10 00:16:51 +0100

    style cleanup

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -4,18 +4,7 @@ import './background.scss';
 const Background = () => {
    return (
       <div className='background'>
-         <div className='deco'>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-         </div>
+        
       </div>
    );
 };
